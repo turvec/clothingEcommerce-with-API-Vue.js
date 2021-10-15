67ea56f1fa9dd02bd6b89f460038d1bc5e7423e2
turvec <okaforturvec@gmail.com>
2021-10-15 04:42:07 -0700

    setup

---
+++ src/components/Arrival.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="feature-category-area">
+        <div class="section-title mb-30">
+            <div class="title-icon">
+                <i class="fa fa-gift"></i>
+            </div>
+            <h3>new arrivals</h3>
+        </div>
+        <!-- section title end -->
+        <!-- featured category start -->
+        <div class="container">
+            <div class="featured-carousel-active2 row slick-arrow-style" data-row="2">
+                <!-- product single item start -->
+                <Product :x="product" v-for="product in products" :key="product.id" />
+                <!-- product single item end -->
+                <!-- product single item end -->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Product from '@/components/Product.vue';
+    import axios from 'axios';
+    export default {
+        components: {
+            Product,
+        },
+        data() {
+            return {
+                products: ""
+            }
+        },
+        mounted() {
+            this.getProduct();
+        },
+        methods: {
+            getProduct() {
+                axios.get('https://fakestoreapi.com/products/category/jewelery')
+                    .then((response) => {
+                        console.log(response);
+                        this.products = response.data
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    })
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
